4eb1e603b1acdaf0a5ff541d7159b52a6aba889c
Alex <alex.bunting314@gmail.com>
2020-04-25 16:04:24 +1000

    Add documentation comments

--- lib/src/document.dart
+++ lib/src/document.dart
@@ -1,7 +1,7 @@
 part of firestore_orm;
 
-// Implement this class to automatically have the path and document Id populated when
-// using [parseSnapshots]
+/// Implement this class to automatically have the path and document Id populated when
+/// using [parseSnapshots] in [DocumentReference] or [Query]
 abstract class FirestoreDocument {
   String get path;
   String get documentId;
@@ -27,20 +27,33 @@ class DocumentReference {
 
   DocumentReference(this._reference);
 
+  /// The path of the collection which contains this document. 
   String get path => _reference.path;
 
+  /// Firestore ID of the document
   String get documentID => _reference.documentID;
 
+  /// Always returns a document snapshot even if the document does not yet exist. Check data = null
+  /// to determine if a document does not exist in Firestore
   Future<DocumentSnapshot> get() async => DocumentSnapshot(await _reference.get());
 
+  /// Get the stream of document update events from Firestore. The Data stream is cached by 
+  /// default, so if this document has been accessed in the past, the last-delivered value
+  /// will be available immediately
   DataStream<DocumentSnapshot> snapshots() => _getSnapshots();
 
+  /// Parse the data in the firestore document into an object. 
   DataStream<T> parseSnapshots<T>(JsonParser<T> parser) => _parseSnapshots(parser);
 
+  /// Set data on this document by serializing the values from an object. The object must have a
+  /// method called 'toJson' which returns a Map<String, dynamic> containing the data. All fields
+  /// of the object will be updated. To update only specific fields in the document, use [setValues]
   Future setData(dynamic data, {bool merge = false}) {
     return _reference.setData(_objectToFirestore(data), merge: merge);
   }
 
+  /// Directly set the values of fields on this document. Allows individual field updates without
+  /// having to update the entire object using [setData]
   Future setValues(Map<String, dynamic> values, {bool merge = false}) {
     return _reference.setData(_valueToFirestore(values), merge: merge);
   }
